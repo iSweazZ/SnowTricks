12cee917d2b5f93e34fe46825e3ce932fa570479
iSweazZ <wissem26500@gmail.com>
2023-07-09 13:33:33 +0200

    create first route

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\TrickCategory;
 use App\Entity\Tricks;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,15 +12,14 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for ($i = 1; $i <= 50; $i++)
-        {
+        for ($i = 1; $i <= 50; $i++) {
             $faker = Faker\Factory::create();
             $trick = new Tricks();
             $trick->setName("test " . $i)
                 ->setPicture("testImg" . $i)
                 ->setDescription($faker->address())
                 ->setPublisher($faker->name())
-                ->setCategory($faker->freeEmail());
+                ->setCategory(TrickCategory::toto);
 
             $manager->persist($trick);
         }
