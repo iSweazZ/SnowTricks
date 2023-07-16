4a67a06fa368b0b841238c238503fccf483706f7
iSweazZ <wissem26500@gmail.com>
2023-07-16 14:40:38 +0200

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,7 +14,6 @@ class HomeController extends AbstractController
     public function index(TricksRepository $tricksRepository): Response
     {
         $tricks = $tricksRepository->findAll();
-        dd($tricks);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
