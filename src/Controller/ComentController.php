2753581f90245c4e1d1cf21a88f4fee2eea41b01
iSweazZ <wissem26500@gmail.com>
2023-08-21 05:51:10 +0200

    update

--- src/Controller/ComentController.php
+++ src/Controller/ComentController.php
@@ -3,21 +3,24 @@
 namespace App\Controller;
 
 use App\Entity\Coments;
+use App\Entity\Tricks;
 use App\Repository\ComentsRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ComentController extends AbstractController
 {
-    #[Route('/coment', name: 'app_coment')]
-    public function store(): int
+    #[Route('/trick/{id}/comment', name: 'app_comment',methods: ['POST'])]
+    public function store(#[MapEntity(id:'id')] Tricks $trick): int
     {
+        $form = $this->createForm();
         $coment = new Coments();
-        //$coment->setTrick();
+        $coment->setText();
     }
 
-    #[Route('/coment/delete', name: 'app_coment_delete')]
+    #[Route('/coment/delete', name: 'app_comment_delete')]
     public function delete(): int
     {
 
